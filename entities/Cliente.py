393d4b34c9731ff1189e2948fb4f73cd8af4dc42
Guilherme Mancini <guilhermemancinidsb@gmail.com>
2024-10-05 16:40:39 -0300

    close issue #1

--- entities/Cliente.py
+++ entities/Cliente.py
@@ -1,1 +1,10 @@
 #arquivo para classe cliente
+class Cliente:
+    def __init__(self, cpf, nome, idade, email):
+        self.cpf=cpf
+        self.nome=nome
+        self.idade=idade
+        self.email=email
+    
+    def __str__(self):
+        return f"Nome: {self.nome} | Cpf: {self.cpf} | Idade: {self.idade} | Email: {self.email}"
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 #Arquivo principal
+from entities.Cliente import Cliente
+
+cliente = Cliente("054.467.344-56", "Ana Beatriz", 32, "ana@gmail.com")
+print(cliente)
